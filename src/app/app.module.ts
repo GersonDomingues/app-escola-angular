f7cb66171b2654f77b1e1a2564fe82fecaac86ad
unknown <gersondsnetto@gmail.com>
2020-12-07 11:38:47 -0300

    "Criado a view home e feito algumas alterações de css"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,9 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list'
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component'
   
 
 @NgModule({
@@ -19,7 +21,8 @@ import {MatListModule} from '@angular/material/list'
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
     
   ],
   imports: [
@@ -28,7 +31,8 @@ import {MatListModule} from '@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
 
   ],
   providers: [],
