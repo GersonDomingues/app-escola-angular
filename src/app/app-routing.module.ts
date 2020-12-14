2bf0acb8fccd9d0ef6ffebd8173bd1a8d4eefb59
unknown <gersondsnetto@gmail.com>
2020-12-14 02:34:49 -0300

    "Criado a  rota disciplinas/update/:id"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { DisciplinaComponent } from './views/disciplina/disciplina.component';
 import { DisciplinaCreateComponent } from './components/disciplina/disciplina-create/disciplina-create.component';
+import { DisciplinaUpdateComponent } from './components/disciplina/disciplina-update/disciplina-update.component';
 
 
 const routes: Routes = [
@@ -18,6 +19,10 @@ const routes: Routes = [
   {
     path: "disciplinas/create",
     component: DisciplinaCreateComponent
+  },
+  {
+    path: "disciplinas/update/:id",
+    component: DisciplinaUpdateComponent
   }
 ];
 
