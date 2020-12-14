f25cd97c9805bcdb2cc48c411e02ade83c7c5cee
unknown <gersondsnetto@gmail.com>
2020-12-14 02:47:55 -0300

    Update disciplina-update.component.ts
    Criado a implementação buscar a informação da disciplina da pagina disciplina-read para com isso  alterar a disciplina .

--- src/app/components/disciplina/disciplina-update/disciplina-update.component.ts
+++ src/app/components/disciplina/disciplina-update/disciplina-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DisciplinaService } from '../disciplina.service';
+import { DisciplinaDTO } from '../disciplinaDTO.model';
 
 @Component({
   selector: 'app-disciplina-update',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisciplinaUpdateComponent implements OnInit {
 
-  constructor() { }
+  obj: DisciplinaDTO= {
+    nome: ""
+  }
+
+  constructor(
+    private disciplinaService: DisciplinaService,
+    private router: Router,
+     private route: ActivatedRoute
+     ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.disciplinaService.readById(id).subscribe(obj => {
+      this.obj = obj;
+      console.log(obj);
+    });
+  }
+
+  updateDisciplina(): void {
+    this.disciplinaService.update(this.obj).subscribe(() => {
+      this.disciplinaService.showMessage("Disciplina atualizada com sucesso..")
+      this.router.navigate(['/disciplinas'])
+    });
+    
+  }
+
+  
+  cancel(): void {
+    this.router.navigate(['/disciplinas'])
   }
 
 }
